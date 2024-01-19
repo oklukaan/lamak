f8d80d770b7d88984f5b0985a5784fcc8a0a3af8
Kaan <kaan.oklu@hotmail.com>
2024-01-19 17:11:51 +0300

    updates

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -5,7 +5,7 @@ import Comments from "@/components/comments/Comments";
 
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`https://lamak.vercel.app/api/posts/${slug}`, {
     cache: "no-store",
   });
 
